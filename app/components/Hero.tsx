b8c69745324701b3acc9587f48e91a3713bc803e
Chukwuebuka Osuji <74817012+Bukasuji@users.noreply.github.com>
2024-04-28 15:26:49 +0100

    added black coover to hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,17 +3,16 @@ import Navbar from "./Navbar";
 
 export default function Hero() {
   return (
-      <div className="max-w-screen min-w-screen bg-cover bg-[url('/assets/rectangle1.jpeg')]">
-
-        <div className="flex flex-col text-white justify-center items-center h-screen">
-           <p className="text-2xl md:text-4xl font-medium ">Welcome to</p>
-           <p className="text-2xl sm:text-3xl md:text-6xl font-medium">FAITH TABERNACLE</p>
-           <p className="text-3xl md:text-6xl font-extrabold">ICT GROUP</p>
-
-           <div className="bg-red-500 text-white text-2xl text-semibold border border-[#939393] border-4 rounded-2xl py-2.5 px-5 mt-8">
-              JOIN US TODAY
-           </div>
+    <div className="relative max-w-screen min-w-screen bg-cover bg-[url('/assets/rectangle1.jpeg')]">
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="flex flex-col text-white justify-center items-center h-screen relative">
+        <p className="text-2xl md:text-4xl font-medium">Welcome to</p>
+        <p className="text-2xl sm:text-3xl md:text-6xl font-medium">FAITH TABERNACLE</p>
+        <p className="text-3xl md:text-6xl font-extrabold">ICT GROUP</p>
+        <div className="bg-red-500 text-white text-2xl text-semibold border border-[#939393] border-4 rounded-2xl py-2.5 px-5 mt-8">
+          JOIN US TODAY
         </div>
       </div>
+    </div>
   );
 }
